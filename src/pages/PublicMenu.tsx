cebb980e368b1e97817fab936bd50eb409805dea
agilqrcode-web <agilqrcode@gmail.com>
2025-09-15 16:23:12 -0300

    Alinhando mesa com cardápio 5

--- src/pages/PublicMenu.tsx
+++ src/pages/PublicMenu.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { usePublicMenu, MenuItem } from '@/hooks/usePublicMenu';
 import { CartProvider } from '@/contexts/CartContext';
 import { Tabs, TabsContent } from '@/components/ui/tabs';
